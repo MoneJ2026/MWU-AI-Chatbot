f1cecc137b83d3bd25f5762d15f7e758e077db25
Mone <monetgirma2018@gmail.com>
2026-07-11 19:18:19 +0300

    Update chatbot engine for multiple JSON files

--- database.py
+++ database.py
@@ -1,22 +1,39 @@
 import json
+import os
 
 
-def load_data():
+DATA_FOLDER = "data"
 
-    with open("university_data.json", "r") as file:
-        data = json.load(file)
 
-    return data
+def load_all_data():
+    knowledge = []
+
+    for file in os.listdir(DATA_FOLDER):
+        if file.endswith(".json"):
+            path = os.path.join(DATA_FOLDER, file)
+
+            with open(path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                knowledge.extend(data)
+
+    return knowledge
+
 
 
 def search_question(question):
 
-    data = load_data()
+    data = load_all_data()
 
     question = question.lower()
 
     for item in data:
-        if item["question"] in question:
-            return item["answer"]
 
-    return "Sorry, information hin argamne."
+        keywords = item.get("keywords", [])
+
+        for keyword in keywords:
+
+            if keyword.lower() in question:
+
+                return item["answer"]["om"]
+
+    return "Dhiifama, odeeffannoo kana hin arganne."
