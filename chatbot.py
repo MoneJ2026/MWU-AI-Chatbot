fc027536903bc336275bb6595145d184ade4bcf5
Mone <monetgirma2018@gmail.com>
2026-07-12 20:25:48 +0300

    Fix chatbot greeting import

--- chatbot.py
+++ chatbot.py
@@ -6,17 +6,41 @@ def get_response(message, language):
 
     message = message.lower().strip()
 
+    print("USER:", message)
+    print("LANG:", language)
+
+
     # Greeting
     if message in GREETINGS:
-        return GREETINGS[message].get(language, GREETINGS[message]["om"])
+
+        print("GREETING FOUND")
+
+        return GREETINGS[message].get(
+            language,
+            GREETINGS[message]["om"]
+        )
+
 
     # Thanks
     if message in THANKS:
-        return THANKS[message].get(language, THANKS[message]["om"])
+
+        return THANKS[message].get(
+            language,
+            THANKS[message]["om"]
+        )
+
 
     # Goodbye
     if message in GOODBYE:
-        return GOODBYE[message].get(language, GOODBYE[message]["om"])
 
-    # University Knowledge Base
-    return search_question(message, language)
+        return GOODBYE[message].get(
+            language,
+            GOODBYE[message]["om"]
+        )
+
+
+    # University information
+    return search_question(
+        message,
+        language
+    )
