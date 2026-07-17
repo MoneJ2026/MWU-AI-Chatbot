bc37bdcfc71c48c49ad211f5b8b16019db60f4a9
Mone <monetgirma2018@gmail.com>
2026-07-17 19:27:12 +0300

    Integrate ML prediction into chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,67 +1,65 @@
-from faq import search_question
-
+from database import search_question
+from ML.predict import predict_intent
 from memory import remember, get_memory
-
 
 
 def get_response(message, language="en"):
 
     print("CHATBOT RECEIVED:", message)
 
-
     message = message.lower().strip()
 
-
-
+    # ==========================
     # USER MEMORY
-    if message.startswith("maqaan koo"):
+    # ==========================
 
+    if message.startswith("maqaan koo"):
 
         name = message.replace(
             "maqaan koo",
             ""
         ).strip()
 
-
-
         remember(
             "name",
             name
         )
 
-
         return {
-
             "answer": f"Galatoomi {name}, si yaadadha.",
-
             "topic": ""
-
         }
-
-
 
     if message == "maqaan kiyya maal":
 
-
         name = get_memory("name")
 
+        if name:
+            return {
+                "answer": f"Maqaan kee {name} dha.",
+                "topic": ""
+            }
 
         return {
-
-            "answer": f"Maqaan kee {name} dha.",
-
+            "answer": "Maqaa kee amma hin yaadadhu.",
             "topic": ""
-
         }
 
+    # ==========================
+    # MACHINE LEARNING
+    # ==========================
 
+    intent = predict_intent(message)
 
-    # FAQ SEARCH
+    print("PREDICTED INTENT:", intent)
 
-    return search_question(
+    # ==========================
+    # DATABASE SEARCH
+    # ==========================
 
+    result = search_question(
         message,
-
         language
-
     )
+
+    return result
